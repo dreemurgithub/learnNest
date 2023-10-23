0815db479f682d15048b118fc6766aac34878757
LeThanhDat <Le Thanh Dat>
2023-10-23 15:15:42 +0700

    write test for post request

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Request } from 'express';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -15,10 +16,28 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
+    it('should return "Hello"', () => {
       expect(appController.getHello()).toStrictEqual({
         message: 'Hello World!',
       });
+    });
+  });
+  describe('Post message', () => {
+    const req = { body: { word: 'Hello' } } as unknown as Request;
+    it('should return "{message: Hello}!"', () => {
+      expect(appController.sayBack(req)).toStrictEqual({
+        message: 'Hello',
+      });
+    });
+  });
+  describe('Post null', () => {
+    const req = { body: { word: '' } } as unknown as Request;
+    const req1 = { body: { word: null } } as unknown as Request;
+    const req2 = { body: {} } as unknown as Request;
+    it('should return "Hello!"', () => {
+      expect(appController.sayBack(req)).toBe('Bad Request');
+      expect(appController.sayBack(req1)).toBe('Bad Request');
+      expect(appController.sayBack(req2)).toBe('Bad Request');
     });
   });
 });
